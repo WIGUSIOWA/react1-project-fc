5b40384b4475a6afb93fca980f1734e3ef79fe90
Agnieszka Dorosz <aga1606@interia.pl>
2023-08-01 14:28:56 +0200

    chenged to App.css instead of module.css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,17 +2,36 @@ import styles from "./Nav.module.css";
 import Container from "../Container/Container";
 const Nav = () => {
 	return (
-		<nav className={styles.container}>
-			<Container>
-				<p className={styles.brand}>best horses ever company</p>
-				<ul className={styles.listItems}>
+		// <nav className={styles.container}>
+		// 	<Container>
+		// 		<p className={styles.brand}>best horses ever company</p>
+		// 		<ul className={styles.listItems}>
+		// 			<li>
+		// 				<a className={styles.goButton}>o nas</a>
+		// 			</li>
+		// 			<li>oferta</li>
+		// 			<li>kontakt</li>
+		// 		</ul>
+		// 	</Container>
+		// </nav>
+
+		<nav className="navigation">
+			<div className="container">
+				<p className="brand">best horses ever company</p>
+				<ul className="list-items">
 					<li>
-						<a className={styles.goButton}>o nas</a>
+						<a className="go-button" href="#dream-team">
+							o nas
+						</a>
 					</li>
-					<li>oferta</li>
-					<li>kontakt</li>
+					<li>
+						<a className="go-button" href="#our-offer">
+							oferta
+						</a>
+					</li>
+					<li className="disabled-button">kontakt</li>
 				</ul>
-			</Container>
+			</div>
 		</nav>
 	);
 };
