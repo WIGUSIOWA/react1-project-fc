24491dd90f2cf418910f6a52a708f6b588963a32
Agnieszka Dorosz <aga1606@interia.pl>
2023-08-02 23:34:20 +0200

    box hipo fixed

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -5,7 +5,7 @@ const Services = () => {
 		{ id: 1, title: "nowość! mentoring szkolenia", isNew: true },
 		{ id: 2, title: "nauka jazdy konnej lub doskonalenie umiejętności" },
 		{ id: 3, title: "tereny / rajdy" },
-		{ id: 4, title: "hipoterapia", isNew: true },
+		{ id: 4, title: "hipoterapia" },
 		{ id: 5, title: "wakacje w siodle" },
 		{ id: 6, title: "wolontariat" },
 	];
